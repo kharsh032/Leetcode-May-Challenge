1ea75721554d8aa38ea2144ea05375ed4d8dda63
Harsh <kharsh032@gmail.com>
2020-05-13 17:41:57 +0530

    Remove k digits

---
+++ Week-2/Day12. Remove K Digits.cpp
@@ -0,0 +1,56 @@
+Problem Link:- https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3328/
+
+/*
+
+Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+The length of num is less than 10002 and will be ≥ k.
+The given num does not contain any leading zero.
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+*/
+
+
+//Solution
+
+class Solution {
+public:
+    string removeKdigits(string num, int k)
+    {
+        if(num.length()==k)
+              return "0";
+        int kk=num.size()-k;
+        string s;
+        for(int i=0;i<num.length();i++)
+        {
+           while(s.size()>0&&s.back()>num[i]&&k)
+           {
+               s.pop_back();
+               k--;
+           }               
+           s.push_back(num[i]);  
+        }
+       s.erase(kk,-1);
+       int kkk=0;
+		while(kkk<s.length()-1&&s[kkk]=='0')
+            kkk++;
+		//Removing leading zeroes from final string
+        s.erase(0,kkk);
+        return s==""?"0":s;
+    }
+};
